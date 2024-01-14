ae066d9917910f897ac22306bbf01f5ad82021bf
Cody <codybarker.or@gmail.com>
2024-01-14 14:44:11 -0800

    add temp conversion

--- unit_converter.js
+++ unit_converter.js
@@ -2,5 +2,14 @@ function kmToMi(kilometers) {
   return Math.round(kilometers * 0.621371 * 100) / 100;
 }
 
-console.log(kmToMi(25));
+// console.log(kmToMi(25));
 
+function cToF(celcius) {
+  return Math.floor(((celcius * 9/5) + 32) * 10)/10
+}
+
+function fToC(fahrenheit) {
+  return Math.floor((fahrenheit - 32) * 5/9 * 10)/10
+}
+
+console.log(fToC(32))
